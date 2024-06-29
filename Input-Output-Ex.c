ed275dbc8c0960425ec6428b7fdd538e0248d74a
albertchoi8 <165109219+albertchoi8@users.noreply.github.com>
2024-06-29 18:18:01 +0900

    Add files via upload

--- Input-Output-Ex.c
+++ Input-Output-Ex.c
@@ -1,12 +1,4 @@
-/*read a character
-   while (character is not end-of-file indicator)
-     output the character just read
-     read a character
-*/
-
 #include <stdio.h>
-
-/* copy input to output */
 
 int main(){
   int c;
